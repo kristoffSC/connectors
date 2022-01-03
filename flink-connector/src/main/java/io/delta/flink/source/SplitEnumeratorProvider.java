2e6e12557e13522cc8377d0ce301fb2dd08543ae
Krzysztof Chmielewski <krzysiek.chmielewski@gmail.com>
2022-01-03 15:17:47 +0100

    Inject Hadoop Configuration object into Delta's SplitEnumerators using Flink's SerializableConfiguration class.
    Signed-off-by: Krzysztof Chmielewski <krzysztof.chmielewski@getindata.com>

--- flink-connector/src/main/java/io/delta/flink/source/SplitEnumeratorProvider.java
+++ flink-connector/src/main/java/io/delta/flink/source/SplitEnumeratorProvider.java
@@ -6,11 +6,13 @@ import org.apache.flink.api.connector.source.Boundedness;
 import org.apache.flink.api.connector.source.SplitEnumerator;
 import org.apache.flink.api.connector.source.SplitEnumeratorContext;
 import org.apache.flink.core.fs.Path;
+import org.apache.hadoop.conf.Configuration;
 
 public interface SplitEnumeratorProvider extends Serializable {
 
     SplitEnumerator<DeltaSourceSplit, DeltaPendingSplitsCheckpoint<DeltaSourceSplit>>
-        createEnumerator(Path deltaTablePath, SplitEnumeratorContext<DeltaSourceSplit> enumContext);
+        createEnumerator(Path deltaTablePath, Configuration configuration,
+        SplitEnumeratorContext<DeltaSourceSplit> enumContext);
 
     Boundedness getBoundedness();
 
