6beb4cdbb306f2dbe074c504fdb14a2ec88e83cb
kristoffSC <krzysiek.chmielewski@gmail.com>
2022-03-15 12:54:08 +0100

    Update flink-connector/src/main/java/io/delta/flink/source/internal/enumerator/ContinuousDeltaSourceSplitEnumerator.java
    Co-authored-by: Tathagata Das <tathagata.das1565@gmail.com>

--- flink-connector/src/main/java/io/delta/flink/source/internal/enumerator/ContinuousDeltaSourceSplitEnumerator.java
+++ flink-connector/src/main/java/io/delta/flink/source/internal/enumerator/ContinuousDeltaSourceSplitEnumerator.java
@@ -253,7 +253,7 @@ public class ContinuousDeltaSourceSplitEnumerator extends DeltaSourceSplitEnumer
      * <p>
      * The {@link #initialSnapshotVersion} however, which is declared as final in a parent class,
      * and it is shared between all {@link DeltaSourceSplitEnumerator} implementations, represents
-     * an "initial" Snapshot version that this Source starts reading from.
+     * an "initial" snapshot version that this Source starts reading from.
      * <p>
      * For initial Source setup or for recovery without a checkpoint, the condition in this method
      * will be true, setting  {@link #currentSnapshotVersion} to the same value as {@link
