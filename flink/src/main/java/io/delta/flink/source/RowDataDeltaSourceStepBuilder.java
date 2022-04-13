41f9572e02a6114f09b853e6b96b8bc59f3437d3
Krzysztof Chmielewski <krzysiek.chmielewski@gmail.com>
2022-04-13 15:43:18 +0200

    PR 9 Introduce DeltaSourceStepBuilder #2
    Signed-off-by: Krzysztof Chmielewski <krzysztof.chmielewski@getindata.com>

--- flink/src/main/java/io/delta/flink/source/RowDataDeltaSourceStepBuilder.java
+++ flink/src/main/java/io/delta/flink/source/RowDataDeltaSourceStepBuilder.java
@@ -2,10 +2,15 @@ package io.delta.flink.source;
 
 import java.util.List;
 
+import io.delta.flink.source.DeltaSourceBuilderSteps.BuildStep;
+import io.delta.flink.source.DeltaSourceBuilderSteps.HadoopConfigurationStep;
+import io.delta.flink.source.DeltaSourceBuilderSteps.TableColumnNameStep;
+import io.delta.flink.source.DeltaSourceBuilderSteps.TableColumnTypeStep;
 import io.delta.flink.source.DeltaSourceBuilderSteps.TablePathStep;
 import io.delta.flink.source.internal.DeltaSourceConfiguration;
 import io.delta.flink.source.internal.DeltaSourceStepBuilder;
 import io.delta.flink.source.internal.state.DeltaSourceSplit;
+import org.apache.flink.core.fs.Path;
 import org.apache.flink.formats.parquet.ParquetColumnarRowInputFormat;
 import org.apache.flink.table.data.RowData;
 import org.apache.flink.table.types.logical.LogicalType;
@@ -25,8 +30,97 @@ public class RowDataDeltaSourceStepBuilder extends DeltaSourceStepBuilder<RowDat
         return new RowDataDeltaSourceStepBuilder();
     }
 
+    @Override
+    public TableColumnNameStep<RowData> tablePath(Path tablePath) {
+        return super.tablePath(tablePath);
+    }
+
     /**
-     * Builds an instance of {@link DeltaSource} that will produce elements of {@link RowData} type.
+     * Doc from the implementation
+     */
+    @Override
+    public TableColumnTypeStep<RowData> columnNames(String[] columnNames) {
+        return super.columnNames(columnNames);
+    }
+
+    @Override
+    public HadoopConfigurationStep<RowData> columnTypes(LogicalType[] columnTypes) {
+        return super.columnTypes(columnTypes);
+    }
+
+    @Override
+    public BuildStep<RowData> hadoopConfiguration(Configuration configuration) {
+        return super.hadoopConfiguration(configuration);
+    }
+
+    @Override
+    public BuildStep<RowData> versionAsOf(long snapshotVersion) {
+        return super.versionAsOf(snapshotVersion);
+    }
+
+    @Override
+    public BuildStep<RowData> timestampAsOf(long snapshotTimestamp) {
+        return super.timestampAsOf(snapshotTimestamp);
+    }
+
+    @Override
+    public BuildStep<RowData> startingVersion(long startingVersion) {
+        return super.startingVersion(startingVersion);
+    }
+
+    @Override
+    public BuildStep<RowData> startingTimestamp(long startingTimestamp) {
+        return super.startingTimestamp(startingTimestamp);
+    }
+
+    @Override
+    public BuildStep<RowData> updateCheckIntervalMillis(long updateCheckInterval) {
+        return super.updateCheckIntervalMillis(updateCheckInterval);
+    }
+
+    @Override
+    public BuildStep<RowData> ignoreDeletes(long ignoreDeletes) {
+        return super.ignoreDeletes(ignoreDeletes);
+    }
+
+    @Override
+    public BuildStep<RowData> ignoreChanges(long ignoreChanges) {
+        return super.ignoreChanges(ignoreChanges);
+    }
+
+    @Override
+    public BuildStep<RowData> option(String optionName, String optionValue) {
+        return super.option(optionName, optionValue);
+    }
+
+    @Override
+    public BuildStep<RowData> option(String optionName, boolean optionValue) {
+        return super.option(optionName, optionValue);
+    }
+
+    @Override
+    public BuildStep<RowData> option(String optionName, int optionValue) {
+        return super.option(optionName, optionValue);
+    }
+
+    @Override
+    public BuildStep<RowData> option(String optionName, long optionValue) {
+        return super.option(optionName, optionValue);
+    }
+
+    @Override
+    public BuildStep<RowData> partitions(List<String> partitions) {
+        return super.partitions(partitions);
+    }
+
+    @Override
+    public BuildStep<RowData> continuousMode() {
+        return super.continuousMode();
+    }
+
+    /**
+     * Builds an instance of {@link DeltaSource} that will produce elements of {@link RowData}
+     * type.
      */
     @Override
     public DeltaSource<RowData> build() {
