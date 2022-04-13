432b38d00a18ba2ff3060ff3133ab497e1211d93
Krzysztof Chmielewski <krzysztof.chmielewski@getindata.com>
2022-04-13 15:40:41 +0200

    PR 9 Introduce DeltaSourceStepBuilder #2
    Signed-off-by: Krzysztof Chmielewski <krzysztof.chmielewski@getindata.com>

--- flink/src/main/java/io/delta/flink/source/RowDataDeltaSourceStepBuilder.java
+++ flink/src/main/java/io/delta/flink/source/RowDataDeltaSourceStepBuilder.java
@@ -2,15 +2,10 @@ package io.delta.flink.source;
 
 import java.util.List;
 
-import io.delta.flink.source.DeltaSourceBuilderSteps.BuildStep;
-import io.delta.flink.source.DeltaSourceBuilderSteps.HadoopConfigurationStep;
-import io.delta.flink.source.DeltaSourceBuilderSteps.TableColumnNameStep;
-import io.delta.flink.source.DeltaSourceBuilderSteps.TableColumnTypeStep;
 import io.delta.flink.source.DeltaSourceBuilderSteps.TablePathStep;
 import io.delta.flink.source.internal.DeltaSourceConfiguration;
 import io.delta.flink.source.internal.DeltaSourceStepBuilder;
 import io.delta.flink.source.internal.state.DeltaSourceSplit;
-import org.apache.flink.core.fs.Path;
 import org.apache.flink.formats.parquet.ParquetColumnarRowInputFormat;
 import org.apache.flink.table.data.RowData;
 import org.apache.flink.table.types.logical.LogicalType;
@@ -28,142 +23,6 @@ public class RowDataDeltaSourceStepBuilder extends DeltaSourceStepBuilder<RowDat
      */
     public static TablePathStep<RowData> stepBuilder() {
         return new RowDataDeltaSourceStepBuilder();
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public TableColumnNameStep<RowData> tablePath(Path tablePath) {
-        return super.tablePath(tablePath);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public TableColumnTypeStep<RowData> columnNames(String[] columnNames) {
-        return super.columnNames(columnNames);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public HadoopConfigurationStep<RowData> columnTypes(LogicalType[] columnTypes) {
-        return super.columnTypes(columnTypes);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public BuildStep<RowData> hadoopConfiguration(Configuration configuration) {
-        return super.hadoopConfiguration(configuration);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public BuildStep<RowData> versionAsOf(long snapshotVersion) {
-        return super.versionAsOf(snapshotVersion);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public BuildStep<RowData> timestampAsOf(long snapshotTimestamp) {
-        return super.timestampAsOf(snapshotTimestamp);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public BuildStep<RowData> startingVersion(long startingVersion) {
-        return super.startingVersion(startingVersion);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public BuildStep<RowData> startingTimestamp(long startingTimestamp) {
-        return super.startingTimestamp(startingTimestamp);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public BuildStep<RowData> updateCheckIntervalMillis(long updateCheckInterval) {
-        return super.updateCheckIntervalMillis(updateCheckInterval);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public BuildStep<RowData> ignoreDeletes(long ignoreDeletes) {
-        return super.ignoreDeletes(ignoreDeletes);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public BuildStep<RowData> ignoreChanges(long ignoreChanges) {
-        return super.ignoreChanges(ignoreChanges);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public BuildStep<RowData> option(String optionName, String optionValue) {
-        return super.option(optionName, optionValue);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public BuildStep<RowData> option(String optionName, boolean optionValue) {
-        return super.option(optionName, optionValue);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public BuildStep<RowData> option(String optionName, int optionValue) {
-        return super.option(optionName, optionValue);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public BuildStep<RowData> option(String optionName, long optionValue) {
-        return super.option(optionName, optionValue);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public BuildStep<RowData> partitions(List<String> partitions) {
-        return super.partitions(partitions);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public BuildStep<RowData> continuousMode() {
-        return super.continuousMode();
     }
 
     /**
