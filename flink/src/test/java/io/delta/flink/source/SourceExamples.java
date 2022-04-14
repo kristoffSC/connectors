810e78ee3a91c7d74fd9730609542aa8faad8ae6
Krzysztof Chmielewski <krzysztof.chmielewski@getindata.com>
2022-04-14 13:02:51 +0200

    PR 9 Examples for DeltaSource Builder
    Signed-off-by: Krzysztof Chmielewski <krzysztof.chmielewski@getindata.com>

---
+++ flink/src/test/java/io/delta/flink/source/SourceExamples.java
@@ -0,0 +1,90 @@
+package io.delta.flink.source;
+
+import io.delta.flink.sink.utils.DeltaSinkTestUtils;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.types.logical.CharType;
+import org.apache.flink.table.types.logical.IntType;
+import org.apache.flink.table.types.logical.LogicalType;
+
+public class SourceExamples {
+
+    protected static final LogicalType[] COLUMN_TYPES =
+        {new CharType(), new CharType(), new IntType()};
+
+    protected static final String[] COLUMN_NAMES = {"name", "surname", "age"};
+
+    public void builderBounded() {
+        DeltaSource<RowData> source = DeltaSource.forRowDataBuilder(
+                new Path("s3://some/path"),
+                COLUMN_NAMES,
+                COLUMN_TYPES,
+                DeltaSinkTestUtils.getHadoopConf())
+            .build();
+    }
+
+    public void stepBuilderBounded() {
+        DeltaSource<RowData> source = DeltaSource.forRowDataStepBuilder()
+            .tablePath(new Path("s3://some/path"))
+            .columnNames(COLUMN_NAMES)
+            .columnTypes(COLUMN_TYPES)
+            .hadoopConfiguration(DeltaSinkTestUtils.getHadoopConf())
+            .build();
+    }
+
+    public void builderContinuous() {
+        DeltaSource<RowData> source = DeltaSource.forRowDataBuilder(
+                new Path("s3://some/path"),
+                COLUMN_NAMES,
+                COLUMN_TYPES,
+                DeltaSinkTestUtils.getHadoopConf())
+            .continuousMode()
+            .build();
+    }
+
+    public void stepBuilderContinuous() {
+        DeltaSource<RowData> source = DeltaSource.forRowDataStepBuilder()
+            .tablePath(new Path("s3://some/path"))
+            .columnNames(COLUMN_NAMES)
+            .columnTypes(COLUMN_TYPES)
+            .hadoopConfiguration(DeltaSinkTestUtils.getHadoopConf())
+            .continuousMode()
+            .build();
+    }
+
+    // Starting from here all examples are using DeltaSourceBuilder and not DeltaSourceStepBuilder
+    // In both implementations, options are defined in the same way.
+    public void builderBoundedPublicOption() {
+        DeltaSource<RowData> source = DeltaSource.forRowDataBuilder(
+                new Path("s3://some/path"),
+                COLUMN_NAMES,
+                COLUMN_TYPES,
+                DeltaSinkTestUtils.getHadoopConf())
+            .ignoreChanges(true)
+            .build();
+    }
+
+    public void builderContinuousPublicOption() {
+        DeltaSource<RowData> source = DeltaSource.forRowDataBuilder(
+                new Path("s3://some/path"),
+                COLUMN_NAMES,
+                COLUMN_TYPES,
+                DeltaSinkTestUtils.getHadoopConf())
+            .ignoreChanges(true)
+            .continuousMode()
+            //.ignoreChanges(true)
+            .build();
+    }
+
+    public void builderContinuousNonPublicOption() {
+        DeltaSource<RowData> source = DeltaSource.forRowDataBuilder(
+                new Path("s3://some/path"),
+                COLUMN_NAMES,
+                COLUMN_TYPES,
+                DeltaSinkTestUtils.getHadoopConf())
+            .continuousMode()
+            .option("parquetBatchSize", 1024)
+            .build();
+    }
+
+}
