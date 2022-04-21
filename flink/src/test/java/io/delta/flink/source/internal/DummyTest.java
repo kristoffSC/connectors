00631321e8b13582e672a4d2d124f040436b4e91
Krzysztof Chmielewski <krzysztof.chmielewski@getindata.com>
2022-04-21 21:55:11 +0200

    test
    Signed-off-by: Krzysztof Chmielewski <krzysztof.chmielewski@getindata.com>

---
+++ flink/src/test/java/io/delta/flink/source/internal/DummyTest.java
@@ -0,0 +1,17 @@
+package io.delta.flink.source.internal;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+
+    @Test
+    public void foo() {
+        System.out.println("hello");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void foo2() {
+        System.out.println("Hello Junit 5");
+    }
+}
