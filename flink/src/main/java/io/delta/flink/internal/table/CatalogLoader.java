224edda59b05317632bbf725651cb71b1271953c
Krzysztof Chmielewski <krzysiek.chmielewski@gmail.com>
2023-02-07 18:48:12 +0100

    Flink_DeltaCatalog_PoC - WIP - load decorated catalog, not supporting Hive yet.
    Signed-off-by: Krzysztof Chmielewski <krzysiek.chmielewski@gmail.com>

--- flink/src/main/java/io/delta/flink/internal/table/CatalogLoader.java
+++ flink/src/main/java/io/delta/flink/internal/table/CatalogLoader.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 import org.apache.flink.table.catalog.Catalog;
 import org.apache.flink.table.catalog.GenericInMemoryCatalogFactory;
-import org.apache.flink.table.catalog.hive.factories.HiveCatalogFactory;
+import org.apache.flink.table.catalog.exceptions.CatalogException;
 import org.apache.flink.table.factories.CatalogFactory.Context;
 import org.apache.hadoop.conf.Configuration;
 
@@ -43,10 +43,17 @@ public interface CatalogLoader extends Serializable {
 
         @Override
         public Catalog createCatalog(Context context) {
-            // We had to add extra dependency for this
+            // We had to add extra dependency to have access to HiveCatalogFactory.
             // "org.apache.flink" % "flink-connector-hive_2.12" % flinkVersion % "provided",
             // "org.apache.flink" % "flink-table-planner_2.12" % flinkVersion % "provided",
-            return new HiveCatalogFactory().createCatalog(context);
+            // and remove "org.apache.flink" % "flink-table-test-utils" % flinkVersion % "test",
+            // but this causes delta CI to fail for scala 2.11.12.
+            // https://github.com/delta-io/connectors/actions/runs/4115104278/jobs/7103364080
+            // Maybe we should be able to use flink-table-test-utils for Hive dependencies if
+            // this would be resolved after https://issues.apache.org/jira/browse/FLINK-27786.
+            // returning null for now, needs further discussion.
+            // return new HiveCatalogFactory().createCatalog(context);
+            throw new CatalogException("Hive decorated catalog is not yet supported.");
         }
     }
 
