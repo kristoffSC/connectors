5d9a6babb4d0b39f10906ba3cd8c6482f5e3b1f2
Krzysztof Chmielewski <krzysiek.chmielewski@gmail.com>
2023-03-14 18:27:49 +0100

    [FlinkSQL_PR_9] - Delta Catalog - Adding Flink's Hive catalog as decorated catalog.
    Signed-off-by: Krzysztof Chmielewski <krzysiek.chmielewski@gmail.com>

--- flink/src/main/java/io/delta/flink/internal/table/DeltaCatalogFactory.java
+++ flink/src/main/java/io/delta/flink/internal/table/DeltaCatalogFactory.java
@@ -1,11 +1,15 @@
 package io.delta.flink.internal.table;
 
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
 import org.apache.flink.configuration.ConfigOption;
 import org.apache.flink.configuration.ConfigOptions;
 import org.apache.flink.configuration.GlobalConfiguration;
 import org.apache.flink.table.catalog.Catalog;
 import org.apache.flink.table.catalog.CommonCatalogOptions;
-import org.apache.flink.table.catalog.GenericInMemoryCatalog;
+import org.apache.flink.table.catalog.exceptions.CatalogException;
 import org.apache.flink.table.factories.CatalogFactory;
 import org.apache.hadoop.conf.Configuration;
 
@@ -18,6 +22,23 @@ import org.apache.hadoop.conf.Configuration;
  * instance.
  */
 public class DeltaCatalogFactory implements CatalogFactory {
+
+    /**
+     * Option to choose what should be the decorated catalog type.
+     */
+    public static final String CATALOG_TYPE = "catalog-type";
+
+    /**
+     * Value for "catalog-type" catalog option that will make Delta Catalog use Flink's Hive Catalog
+     * as its decorated catalog.
+     */
+    public static final String CATALOG_TYPE_HIVE = "hive";
+
+    /**
+     * Value for "catalog-type" catalog option that will make Delta Catalog use Flink's In-memory
+     * catalog as its decorated catalog.
+     */
+    public static final String CATALOG_TYPE_IN_MEMORY = "in-memory";
 
     /**
      * Property with default database name used for metastore entries.
@@ -35,8 +56,27 @@ public class DeltaCatalogFactory implements CatalogFactory {
      */
     @Override
     public Catalog createCatalog(Context context) {
-        // TODO FlinkSQL_PR_9 - inject proper decorated catalog based on catalog properties
-        Catalog decoratedCatalog = new GenericInMemoryCatalog(context.getName(), "default");
+        Map<String, String> originalOptions = context.getOptions();
+
+        // Since we want to add extra options here, to avoid any unintentional mutation of the
+        // input context object and causing added options to leak out we are creating a working
+        // copy of original option map. We are playing safe here, making sure nothing, that has
+        // Delta Catalog scope will leak out.
+        Map<String, String> deltaContextOptions = new HashMap<>(originalOptions);
+
+        // Making sure that decorated catalog will use the same name for default database.
+        if (!deltaContextOptions.containsKey(CommonCatalogOptions.DEFAULT_DATABASE_KEY)) {
+            deltaContextOptions.put(DEFAULT_DATABASE.key(), DEFAULT_DATABASE.defaultValue());
+        }
+
+        DeltaCatalogContext deltaCatalogContext = new DeltaCatalogContext(
+            context.getName(),
+            deltaContextOptions,
+            context.getConfiguration(),
+            context.getClassLoader()
+        );
+
+        Catalog decoratedCatalog = createDecoratedCatalog(deltaCatalogContext);
         Configuration hadoopConfiguration =
             HadoopUtils.getHadoopConfiguration(GlobalConfiguration.loadConfiguration());
         return new CatalogProxy(context.getName(), "default", decoratedCatalog,
@@ -48,4 +88,20 @@ public class DeltaCatalogFactory implements CatalogFactory {
         return "delta-catalog";
     }
 
+    private Catalog createDecoratedCatalog(Context context) {
+
+        Map<String, String> options = context.getOptions();
+        String catalogType = options.getOrDefault(CATALOG_TYPE, CATALOG_TYPE_IN_MEMORY);
+
+        switch (catalogType.toLowerCase(Locale.ENGLISH)) {
+            case CATALOG_TYPE_HIVE:
+                return CatalogLoader.hive().createCatalog(context);
+            case CATALOG_TYPE_IN_MEMORY:
+                return CatalogLoader.inMemory().createCatalog(context);
+            default:
+                throw new CatalogException("Unknown catalog-type: " + catalogType +
+                    " (Must be 'hive' or 'inMemory')");
+        }
+
+    }
 }
